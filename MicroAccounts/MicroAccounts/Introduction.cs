3113ea3b20baeaa9a698b45962d545b57a4275af
MjNathani <mjnathani29@gmail.com>
2018-08-23 05:18:57 -0700

    Changes in dashboard

---
+++ MicroAccounts/MicroAccounts/Introduction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MicroAccounts
+{
+    public partial class Introduction : UserControl
+    {
+        public Introduction()
+        {
+            InitializeComponent();
+        }
+
+        private void Introduction_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
--- MicroAccounts/MicroAccounts/Program.cs
+++ MicroAccounts/MicroAccounts/Program.cs
@@ -16,7 +16,7 @@ namespace MicroAccounts
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Dasboard());
         }
     }
 }
